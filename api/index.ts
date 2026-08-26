4788721a6d64103effc9e4a6447d3f83a7f094f0
Diego Vieira <dvdiego87@gmail.com>
2026-08-26 06:26:04 -0300

    fix: restore api/index.ts with includeFiles config

---
+++ api/index.ts
@@ -0,0 +1,7 @@
+import type { IncomingMessage, ServerResponse } from "node:http";
+import { app } from "../src/app.ts";
+
+export default async function handler(req: IncomingMessage, res: ServerResponse) {
+  await app.ready();
+  app.server.emit("request", req, res);
+}
